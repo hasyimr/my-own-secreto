6d53502027d8d5912ffbbbf29a96eb65599a498f
Hasyim Rahmadi <hasyimr90@gmail.com>
2022-02-22 09:42:13 +0700

    change uri database

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
 db = SQLAlchemy(app)
 
 
